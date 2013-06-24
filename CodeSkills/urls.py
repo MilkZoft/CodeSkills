620f63337478de9d4d9fc8194bd3bc0149e00923
Carlos Santana <carlos@milkzoft.com>
2013-06-23 19:08:49 -0500

    Urls

--- CodeSkills/urls.py
+++ CodeSkills/urls.py
@@ -14,4 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^public/media/(?P.*)$', 'django.views.static.serve', {'document_root':'settings.MEDIA_ROOT'}),
+    url(r'^public/static/(?P.*)$', 'django.views.static.serve', {'document_root':'settings.STATIC_ROOT'}),
 )
