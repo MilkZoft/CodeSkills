ea7cc21e00a883707778ea00263051ffeed5fe2e
Carlos Santana <carlos@milkzoft.com>
2013-06-24 02:22:43 -0500

    settings

--- apps/bank/urls.py
+++ apps/bank/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
-urlspatterns = patterns(
+urlpatterns = patterns(
 	'apps.bank.views',
 	url(r'^$', 'index', name = 'index'),
 	url(r'^bank/questions/show/(?P<id>\d+)/$', 'showQuestion', name = 'question'),
