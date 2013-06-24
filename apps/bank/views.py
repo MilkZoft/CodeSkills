8422091396d80ab847570c651a89799d0563799e
Carlos Santana <carlos@milkzoft.com>
2013-06-24 02:23:16 -0500

    settings

--- apps/bank/views.py
+++ apps/bank/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from apps.bank import Questions, Technologies
+from apps.bank.models import Questions, Technologies
 from django.http import HttpResponseRedirect
 from apps.bank import *
 
