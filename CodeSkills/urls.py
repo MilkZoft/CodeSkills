10079488fd6098423f9ff92d354d4946d143db7b
Carlos Santana <carlos@milkzoft.com>
2013-06-23 19:19:34 -0500

    Static moved

--- CodeSkills/urls.py
+++ CodeSkills/urls.py
@@ -14,6 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^public/media/(?P.*)$', 'django.views.static.serve', {'document_root':'settings.MEDIA_ROOT'}),
-    url(r'^public/static/(?P.*)$', 'django.views.static.serve', {'document_root':'settings.STATIC_ROOT'}),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'settings.MEDIA_ROOT'}),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'settings.STATIC_ROOT'}),
 )
