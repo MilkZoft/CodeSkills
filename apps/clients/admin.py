4e5fa731fc5df5acbc00b21ca66f9e75c96d9380
Carlos Santana <carlos@milkzoft.com>
2013-06-24 19:57:17 -0500

    Clients app added

--- apps/clients/admin.py
+++ apps/clients/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from apps.clients.models import *
-import pprint
+from apps.clients.models import Countries, Cities, Companies
 
 class CountriesAdmin(admin.TabularInline):
 	model = Countries
