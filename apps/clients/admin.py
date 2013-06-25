509db5d19cbeadd78ed2a415cb594ad5f7087393
Carlos Santana <carlos@milkzoft.com>
2013-06-24 19:59:56 -0500

    Clients app added

--- apps/clients/admin.py
+++ apps/clients/admin.py
@@ -5,15 +5,4 @@ class CountriesAdmin(admin.TabularInline):
 	model = Countries
 	extra = 0
 
-class CitiesAdmin(admin.TabularInline):
-	model = Cities
-	extra = 0
-
-class CompaniesAdmin(admin.ModelAdmin):
-	list_display = ('id', 'company',)
-	search_fields = ['company']
-	extra = 1
-
-admin.site.register(Companies, CompaniesAdmin)
-admin.site.register(CitiesAdmin)
 admin.site.register(CountriesAdmin)
