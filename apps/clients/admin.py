d970c8af9444d240169ced8da48545057117283d
Carlos Santana <carlos@milkzoft.com>
2013-06-25 11:16:11 -0500

    admin.py fixed

--- apps/clients/admin.py
+++ apps/clients/admin.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from apps.clients.models import Locations, Companies
 
 class CompaniesAdmin(admin.ModelAdmin):
-	list_display = ('company', 'email', 'twitter', 'phone', 'industry', 'location')
+	list_display = ('company', 'email', 'linkToTwitter', 'phone', 'industry', 'location')
 	list_filter = ('industry', 'location',)
 	search_fields = ['company']
 	extra = 0
+
+	def linkToTwitter(self, obj):
+        return u'<a href="http://twitter.com/%s" target="_blank">%s</a>' % (obj.twitter)
 
 admin.site.register(Locations)
 admin.site.register(Companies, CompaniesAdmin)
