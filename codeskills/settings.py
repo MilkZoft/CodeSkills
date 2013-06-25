ba9ff72f29345907e16aa05caf8f23edd3df0d06
Carlos Santana <carlos@milkzoft.com>
2013-06-25 14:10:03 -0500

    .po files

--- codeskills/settings.py
+++ codeskills/settings.py
@@ -33,12 +33,7 @@ TIME_ZONE = 'America/Mexico_City'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en'
-
-LANGUAGES = (
-    ('es', 'Spanish'),
-    ('en', 'English'),
-)
+LANGUAGE_CODE = 'es'
 
 LOCALE_PATHS = (
     '/home/czantany/public_html/CodeSkills/locale', 
