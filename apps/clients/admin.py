43b1056cc111a3a920a8d95f9022414fbdb19ea1
Carlos Santana <carlos@milkzoft.com>
2013-06-24 21:06:22 -0500

    admin fixed

--- apps/clients/admin.py
+++ apps/clients/admin.py
@@ -3,7 +3,7 @@ from apps.clients.models import Locations, Companies
 
 class CompaniesAdmin(admin.ModelAdmin):
 	list_display = ('id', 'company', 'email', 'website', 'twitter', 'facebook', 'phone', 'industry')
-	list_filter = ('industry', 'locations')
+	list_filter = ('industry')
 	search_fields = ['company']
 	extra = 0
 
