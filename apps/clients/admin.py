e613bb723eaaa9b671a99474b73b9c078447edd2
Carlos Santana <carlos@milkzoft.com>
2013-06-25 14:18:06 -0500

    .po files

--- apps/clients/admin.py
+++ apps/clients/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from apps.clients.models import Locations, Companies
+from django.utils.translation import ugettext as _
 
 class CompaniesAdmin(admin.ModelAdmin):
-	list_display = ('company', 'linkToEmail', 'linkToTwitter', 'phone', 'industry', 'location')
+	list_display = ('company', 'linkToEmail', 'linkToTwitter', 'phone', 'industry', 'showLocation')
 	list_filter = ('industry', 'location',)
 	search_fields = ['company']
 	extra = 0
+
+	def showLocation(self, Locations):
+		return Locations.city + ', ' + Locations.country
 
 	def linkToEmail(self, Companies):
 		return '<a href="mailto:' + Companies.email + '">' + Companies.email + '</a>'
@@ -14,9 +18,9 @@ class CompaniesAdmin(admin.ModelAdmin):
 		return '<a href="http://twitter.com/' + Companies.twitter + '" target="_blank">' + Companies.twitter + '</a>'
 
 	linkToEmail.allow_tags = True
-	linkToEmail.short_description = 'Email' 
+	linkToEmail.short_description = _("Email") 
 	linkToTwitter.allow_tags = True
 	linkToTwitter.short_description = 'Twitter' 
-
+	showLocation.short_description = _("Location")
 admin.site.register(Locations)
 admin.site.register(Companies, CompaniesAdmin)
