f4f0ffd6310795a0f31258392a1e8035c2cd7dea
Carlos Santana <carlos@milkzoft.com>
2013-06-25 14:12:55 -0500

    .po files

--- apps/clients/models.py
+++ apps/clients/models.py
@@ -17,17 +17,17 @@ class Locations(models.Model):
 class Companies(models.Model):
 	location = models.ForeignKey(Locations)
 	company  = models.CharField(_('Company'), max_length = 150)
-	contact  = models.TextField(null = True, blank = True, max_length = 100)
-	address  = models.TextField(null = True, blank = True)
-	email    = models.CharField('Email', null = True, blank = True, max_length = 120)
-	skype 	 = models.CharField('Skype', null = True, blank = True, max_length = 60)
-	website  = models.CharField('Website', default = 'http://', null = True, blank = True, max_length = 100)
-	linkedin = models.CharField('Linkedin', null = True, blank = True, max_length = 60)
-	twitter  = models.CharField('Twitter', default = '@', null = True, blank = True, max_length = 60)
-	facebook = models.CharField('Facebook', null = True, blank = True, max_length = 60)
-	phone    = models.CharField('Phone', null = True, blank = True, max_length = 15)
-	mobile   = models.CharField('Mobile', null = True, blank = True, max_length = 15)
-	industry = models.CharField('Industry', default = 'Software', null = True, blank = True, max_length = 100)
+	contact  = models.TextField(_('Contact'), null = True, blank = True, max_length = 100)
+	address  = models.TextField(_('Address'), null = True, blank = True)
+	email    = models.CharField(_('Email'), null = True, blank = True, max_length = 120)
+	skype 	 = models.CharField(_('Skype'), null = True, blank = True, max_length = 60)
+	website  = models.CharField(_('Website'), default = 'http://', null = True, blank = True, max_length = 100)
+	linkedin = models.CharField(_('Linkedin'), null = True, blank = True, max_length = 60)
+	twitter  = models.CharField(_('Twitter'), default = '@', null = True, blank = True, max_length = 60)
+	facebook = models.CharField(_('Facebook'), null = True, blank = True, max_length = 60)
+	phone    = models.CharField(_('Phone'), null = True, blank = True, max_length = 15)
+	mobile   = models.CharField(_('Mobile'), null = True, blank = True, max_length = 15)
+	industry = models.CharField(_('Industry'), default = 'Software', null = True, blank = True, max_length = 100)
 
 	def __unicode__(self):
 		return self.company
