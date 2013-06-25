41aee6ba21afd4cbf122df2198c37f3ff243d306
Carlos Santana <carlos@milkzoft.com>
2013-06-24 20:55:12 -0500

    Clients app added

--- apps/clients/models.py
+++ apps/clients/models.py
@@ -16,15 +16,16 @@ class Locations(models.Model):
 class Companies(models.Model):
 	location  = models.ForeignKey(Locations)
 	company  = models.CharField('Company', max_length = 150)
-	contact  = models.CharField('Contact', default = ' ', null = True, blank = True, max_length = 100)
-	address  = models.TextField(null = True, default = ' ', blank = True)
-	email    = models.CharField('Email', default = ' ', null = True, blank = True, max_length = 120)
-	website  = models.CharField('website', default = ' ', null = True, blank = True, max_length = 100)
-	twitter  = models.CharField('Twitter', default = ' ', null = True, blank = True, max_length = 60)
-	facebook = models.CharField('Facebook', default = ' ', null = True, blank = True, max_length = 60)
-	phone    = models.CharField('Phone', default = ' ', null = True, blank = True, max_length = 15)
-	mobile   = models.CharField('Mobile', default = ' ', null = True, blank = True, max_length = 15)
-	industry = models.CharField('Industry', default = ' ', null = True, blank = True, max_length = 100)
+	contact  = models.CharField('Contact', null = True, blank = True, max_length = 100)
+	address  = models.TextField(null = True, blank = True)
+	email    = models.CharField('Email', null = True, blank = True, max_length = 120)
+	website  = models.CharField('website', null = True, blank = True, max_length = 100)
+	linkedin = models.CharField('Linkedin', null = True, blank = True, max_length = 60)
+	twitter  = models.CharField('Twitter', null = True, blank = True, max_length = 60)
+	facebook = models.CharField('Facebook', null = True, blank = True, max_length = 60)
+	phone    = models.CharField('Phone', null = True, blank = True, max_length = 15)
+	mobile   = models.CharField('Mobile', null = True, blank = True, max_length = 15)
+	industry = models.CharField('Industry', null = True, blank = True, max_length = 100)
 
 	def __unicode__(self):
 		return self.company
