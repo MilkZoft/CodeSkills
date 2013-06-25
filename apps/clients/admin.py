6cbbc8aaef787b17734605fd6fbdf0912c81942f
Carlos Santana <carlos@milkzoft.com>
2013-06-24 20:10:02 -0500

    Clients app added

--- apps/clients/admin.py
+++ apps/clients/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from apps.clients.models import Countries, Cities, Companies
 
 admin.site.register(Countries)
+admin.site.register(Cities)
+admin.site.register(Companies)
