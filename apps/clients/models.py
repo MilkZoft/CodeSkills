0c81aa4995c893317fb78c49e8d6fd9c2796778f
Carlos Santana <carlos@milkzoft.com>
2013-06-24 19:30:08 -0500

    Clients app added

--- apps/clients/models.py
+++ apps/clients/models.py
@@ -2,25 +2,6 @@
 from django.db import models
 from datetime import datetime
 
-class Companies(models.Model):
-	country  = models.ForeignKey(Countries)
-	city     = models.ForeignKey(Cities)
-	company  = models.CharField('Company', max_length = 150)
-	contact  = models.CharField('Contact', max_length = 100)
-	address  = models.TextField()
-	email    = models.CharField('Email', max_length = 120)
-	phone    = models.CharField('Phone', max_length = 15)
-	mobile   = models.CharField('Mobile', max_length = 15)
-	industry = models.CharField('Industry', max_length = 100)
-
-	def __unicode__(self):
-		return self.company
-
-	class Meta:
-		ordering = ('company',)
-		verbose_name = u'Company'
-		verbose_name_plural = u'Companies'
-
 class Countries(models.Model):
 	country = models.CharField('Country', max_length = 50)
 	
@@ -42,3 +23,22 @@ class Cities(models.Model):
 		ordering = ('city',)
 		verbose_name = u'City'
 		verbose_name_plural = u'Cities'	
+		
+class Companies(models.Model):
+	country  = models.ForeignKey(Countries)
+	city     = models.ForeignKey(Cities)
+	company  = models.CharField('Company', max_length = 150)
+	contact  = models.CharField('Contact', max_length = 100)
+	address  = models.TextField()
+	email    = models.CharField('Email', max_length = 120)
+	phone    = models.CharField('Phone', max_length = 15)
+	mobile   = models.CharField('Mobile', max_length = 15)
+	industry = models.CharField('Industry', max_length = 100)
+
+	def __unicode__(self):
+		return self.company
+
+	class Meta:
+		ordering = ('company',)
+		verbose_name = u'Company'
+		verbose_name_plural = u'Companies'
