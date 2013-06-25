719dfa7dbdc3e6d46f8069d47aa8b21e0074b80d
Carlos Santana <carlos@milkzoft.com>
2013-06-24 20:37:59 -0500

    Clients app added

--- apps/clients/models.py
+++ apps/clients/models.py
@@ -16,15 +16,15 @@ class Locations(models.Model):
 class Companies(models.Model):
 	location  = models.ForeignKey(Locations)
 	company  = models.CharField('Company', max_length = 150)
-	contact  = models.CharField('Contact', max_length = 100)
-	address  = models.TextField()
-	email    = models.CharField('Email', max_length = 120)
-	website  = models.CharField('website', max_length = 100)
-	twitter  = models.CharField('Twitter', max_length = 60)
-	facebook = models.CharField('Facebook', max_length = 60)
-	phone    = models.CharField('Phone', max_length = 15)
-	mobile   = models.CharField('Mobile', max_length = 15)
-	industry = models.CharField('Industry', max_length = 100)
+	contact  = models.CharField('Contact', null = True, blank = True, max_length = 100)
+	address  = models.TextField(null = True, blank = True)
+	email    = models.CharField('Email', null = True, blank = True, max_length = 120)
+	website  = models.CharField('website', null = True, blank = True, max_length = 100)
+	twitter  = models.CharField('Twitter', null = True, blank = True, max_length = 60)
+	facebook = models.CharField('Facebook', null = True, blank = True, max_length = 60)
+	phone    = models.CharField('Phone', null = True, blank = True, max_length = 15)
+	mobile   = models.CharField('Mobile', null = True, blank = True, max_length = 15)
+	industry = models.CharField('Industry', null = True, blank = True, max_length = 100)
 
 	def __unicode__(self):
 		return self.company
