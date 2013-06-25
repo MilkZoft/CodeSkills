9d97a540b6f025e39d15d65ae766de3fce58ed48
Carlos Santana <carlos@milkzoft.com>
2013-06-24 21:05:37 -0500

    admin fixed

--- apps/clients/admin.py
+++ apps/clients/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from apps.clients.models import Locations, Companies
 
+class CompaniesAdmin(admin.ModelAdmin):
+	list_display = ('id', 'company', 'email', 'website', 'twitter', 'facebook', 'phone', 'industry')
+	list_filter = ('industry', 'locations')
+	search_fields = ['company']
+	extra = 0
+
 admin.site.register(Locations)
-admin.site.register(Companies)
+admin.site.register(Companies, CompaniesAdmin)
