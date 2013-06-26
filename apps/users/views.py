82031c7651375ef618c387a43151ed2c15b480d3
Carlos Santana <carlos@milkzoft.com>
2013-06-26 18:45:20 -0500

    users app

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,7 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from apps.users.models import Users
-from django.http import HttpResponseRedirect
+from apps.users import *
 
 def login(request):
 	return render_to_response(
