0777016c228ddd09a331b126f539d380f2aa0e75
Carlos Santana <carlos@milkzoft.com>
2013-06-26 18:50:08 -0500

    users app

--- codeskills/urls.py
+++ codeskills/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^users/', include('apps.users.urls')),
+	url(r'^', include('apps.users.urls')),
    	url(r'^bank/', include('apps.bank.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^public/media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'settings.MEDIA_ROOT'}),
