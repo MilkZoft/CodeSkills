b575fb096aa51cfef778f05a3f795c96c148b38f
Carlos Santana <carlos@milkzoft.com>
2013-06-26 18:38:14 -0500

    users app

--- codeskills/urls.py
+++ codeskills/urls.py
@@ -5,7 +5,8 @@ admin.autodiscover()
 
 urlpatterns = patterns(
 	'',
-   	url(r'^', include('apps.bank.urls')),
+	url(r'^users/', include('apps.users.urls')),
+   	url(r'^bank/', include('apps.bank.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^public/media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'settings.MEDIA_ROOT'}),
     url(r'^public/static/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'settings.STATIC_ROOT'}),
