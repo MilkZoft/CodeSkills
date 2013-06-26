750613d4cf9bcb31c4aee6ad1284f3d5368275aa
Carlos Santana <carlos@milkzoft.com>
2013-06-26 18:36:51 -0500

    users app

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns(
-	'apps.users.views',
-	url(r'^$', 'index', name = 'index'),
-	url(r'^bank/questions/show/(?P<id>\d+)/$', 'showQuestion', name = 'question'),
+	'apps.users.views'
+	url(r'^users/login/$', 'login', name = 'user'),
 )
